21a46146c3d6fe598069c16b8513a92920defaa5
SCHTAMP <ylyka0@ya.ru>
2020-07-23 23:44:45 +0300

    Init commit

---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 2155291956;
+name = "Last Stand unit";
+timestamp = 5248992651125085135;
---
+++ mod.cpp
@@ -0,0 +1,5 @@
+author = "LastWar Team";
+name = "Last War Unit";
+picture = "Logo.paa";
+logo = "Logo.paa";
+overview = "LastWar Team";
